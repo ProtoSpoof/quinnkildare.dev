cf1836edd45d974d534ff8f9994848d7403a5555
Quinlan Kildare <quinlankildare@gmail.com>
2022-12-08 00:47:21 -0500

    fix title

--- pages/card.tsx
+++ pages/card.tsx
@@ -1,48 +1,39 @@
 import React from "react";
 import Image from "next/image";
 import styles from "../styles/Card.module.scss";
-import me from "../public/images/me.gif";
 import Link from "next/link";
+import CustomHead from "../components/CustomHead";
+import me from "../public/images/me.gif";
 
 export default function Card() {
 	return (
-		<main className={"flex items-center justify-center h-screen bg-gradient-to-br from-[#fc5c7d] to-[#6a82fb]"}>
-			<div className='grid grid-cols-1 gap-8 bg-black/75 font-semibold text-center rounded-xl shadow-3xl shadow-black p-10 max-w-xs'>
-				{/* <Image src={me} className='mb-3 w-32 h-32 rounded-full shadow-lg mx-auto' alt='product designer'> */}
-				<div
-					className='flex content-center justify-center self-center justify-self-center h-[150px] aspect-square glow-circle'
-					title='image container'
-				>
-					<Image src={me} alt='picture of the author' className='rounded-full' role='presentation' />
-				</div>
-				<div>
-					<h1 className={`text-2xl text-transparent bg-clip-text ${styles.background}`}>Quinlan Kildare</h1>
-					<h3 className='text-sm text-gray-400 '>
-						<em className='text-inherit'>Computer Scientist. Programmer. Engineer.</em>
-					</h3>
-				</div>
-				<p className='text-xs text-gray-300'>
+		<>
+			<CustomHead Title="Card - quinnkildare.dev" Description="Business Card"/>
+			<main className={"flex items-center justify-center h-screen bg-gradient-to-br from-[#fc5c7d] to-[#6a82fb]"}>
+				<div className='grid grid-cols-1 gap-8 bg-black/75 font-semibold text-center rounded-xl shadow-3xl shadow-black p-10 max-w-xs'>
+					{/* <Image src={me} className='mb-3 w-32 h-32 rounded-full shadow-lg mx-auto' alt='product designer'> */}
+					<div
+						className='flex content-center justify-center self-center justify-self-center h-[150px] aspect-square glow-circle'
+						title='image container'
+					>
+						<Image src={me} alt='picture of the author' className='rounded-full' role='presentation' />
+					</div>
+					<div>
+						<h1 className={`text-2xl text-transparent bg-clip-text ${styles.background}`}>Quinlan Kildare</h1>
+						<h3 className='text-sm text-gray-400 '>
+							<em className='text-inherit'>Computer Scientist. Programmer. Engineer.</em>
+						</h3>
+					</div>
+					<p className='text-xs text-gray-300'>
 					Computer Science student at the University of Central Florida with a minor in Intelligent Robotic
 					Systems. Experience with full stack development, as well as developing standalone applications in C
 					and C++.
-				</p>
-				<div className='flex content-center justify-between'>
-					<a
-						className='hover:scale-125 transition-transform'
-						href='https://www.linkedin.com/in/quinlan-kildare-a030781ba/'
-					>
-						<svg
-							className='fill-gray-200'
-							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
-							viewBox='0 0 24 24'
+					</p>
+					<div className='flex content-center justify-between'>
+						<a
+							className='hover:scale-125 transition-transform'
+							href='https://www.linkedin.com/in/quinlan-kildare-a030781ba/'
 						>
-							<path d='M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z' />
-						</svg>
-					</a>
-					<Link href="/">
-						<a className='hover:scale-125 transition-transform'>
 							<svg
 								className='fill-gray-200'
 								xmlns='http://www.w3.org/2000/svg'
@@ -50,30 +41,43 @@ export default function Card() {
 								height='24'
 								viewBox='0 0 24 24'
 							>
-								<path d='M24 10.935v2.131l-8 3.947v-2.23l5.64-2.783-5.64-2.79v-2.223l8 3.948zm-16 3.848l-5.64-2.783 5.64-2.79v-2.223l-8 3.948v2.131l8 3.947v-2.23zm7.047-10.783h-2.078l-4.011 16h2.073l4.016-16z' />
+								<path d='M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z' />
 							</svg>
 						</a>
-					</Link>
+						<Link href="/">
+							<a className='hover:scale-125 transition-transform'>
+								<svg
+									className='fill-gray-200'
+									xmlns='http://www.w3.org/2000/svg'
+									width='24'
+									height='24'
+									viewBox='0 0 24 24'
+								>
+									<path d='M24 10.935v2.131l-8 3.947v-2.23l5.64-2.783-5.64-2.79v-2.223l8 3.948zm-16 3.848l-5.64-2.783 5.64-2.79v-2.223l-8 3.948v2.131l8 3.947v-2.23zm7.047-10.783h-2.078l-4.011 16h2.073l4.016-16z' />
+								</svg>
+							</a>
+						</Link>
 
-					<a className='hover:scale-125 transition-transform' href='https://github.com/ProtoSpoof'>
-						<svg
-							className='fill-gray-200'
-							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
-							viewBox='0 0 24 24'
-						>
-							<path d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z' />
-						</svg>
+						<a className='hover:scale-125 transition-transform' href='https://github.com/ProtoSpoof'>
+							<svg
+								className='fill-gray-200'
+								xmlns='http://www.w3.org/2000/svg'
+								width='24'
+								height='24'
+								viewBox='0 0 24 24'
+							>
+								<path d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z' />
+							</svg>
+						</a>
+					</div>
+					<a
+						className={"bg-[#f7797d] rounded-full hover:scale-110 transition-transform text-[snow] py-2 px-4 text-sm font-medium tracking-wide uppercase"}
+						href='mailto:contact@quinnkildare.dev'
+					>
+					Contact Me
 					</a>
 				</div>
-				<a
-					className={"bg-[#f7797d] rounded-full hover:scale-110 transition-transform text-[snow] py-2 px-4 text-sm font-medium tracking-wide uppercase"}
-					href='mailto:contact@quinnkildare.dev'
-				>
-					Contact Me
-				</a>
-			</div>
-		</main>
+			</main>
+		</>
 	);
 }
