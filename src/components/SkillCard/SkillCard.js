9c8a410a38a1c77f13b6b607732bbe7cd2e42c3b
Quinlan Kildare <quinlankildare@gmail.com>
2023-05-26 02:22:44 -0400

    fix spacing

--- src/components/SkillCard/SkillCard.js
+++ src/components/SkillCard/SkillCard.js
@@ -3,7 +3,7 @@ import styles from '@/styles/components/SkillCard/SkillCard.module.scss';
 export function SkillCard({ name, SkillIcon }) {
   return (
     <div className={styles.outer}>
-        <div className={`${styles.inner} w-24 -mb-7 sm:w-28 sm:-mb-8 md:w-40 md:-mb-11 overflow-hidden`}>
+        <div className={`${styles.inner} w-24 -mb-[1.9rem] sm:w-28 sm:-mb-[2.15rem] md:w-40 md:-mb-[3.1rem] overflow-hidden`}>
             <div className={styles.content}>
                 <SkillIcon size='50%'/>
             </div>
