dd697786ebfb00ca44584626fbf6ac02e5a61587
Quinlan Kildare <quinlankildare@gmail.com>
2023-05-26 02:10:49 -0400

    restyle project cards

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -2,15 +2,17 @@ import styles from '@/styles/components/ProjectCard/ProjectCard.module.scss';
 
 export function ProjectCard({name, summary, link, languages}) {
   return (
-    <a href={link} className={styles.card}>
-        <h2 className='text-2xl'>{name}</h2>
-        <hr />
-        <p className='text-sm'>{summary}</p>
-        <ul className={styles.languages}>
-            {languages.map((language) => {
-                return <li className='text-xs'>{language}</li>;
-            })}
-        </ul>
+    <a href={link} className={styles.outer}>
+        <div className={styles.inner}>
+            <h2 className='text-2xl'>{name}</h2>
+            <hr />
+            <p className='text-sm'>{summary}</p>
+            <ul className={styles.languages}>
+                {languages.map((language) => {
+                    return <li className='text-xs'>{language}</li>;
+                })}
+            </ul>
+        </div>
     </a>
   );
 }
